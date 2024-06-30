Absolutely nailed finite difference numerical issues, and still long distance behaviour is not fixed.

core/average_atom.py
@@ -18,7 +18,7 @@ from hnc.hnc.hnc import Integral_Equation_Solver as IET
 from hnc.hnc.constants import *
 
 from .grids import NonUniformGrid
-from .solvers import jacobi_relaxation, sor, gmres_ilu, tridiagsolve
+from .solvers import jacobi_relaxation, sor, gmres_ilu, tridiagsolve, Ndiagsolve
 from .physics import FermiDirac, ThomasFermi, χ_Lindhard, χ_TF, More_TF_Zbar, Fermi_Energy, n_from_rs, Debye_length
 
 import matplotlib.pyplot as plt
@@ -86,7 +86,7 @@ class NeutralPseudoAtom(Atom):
 	"""
 	def __init__(self, Z, A, Ti, Te, rs, R, initialize=True, gradient_correction=None, Weizsacker_λ = 1, μ_init = None, Zstar_init = 'More', 
 		rmin=2e-2, Npoints=100, iet_R_over_rs = None, iet_N_bins = 2000, name='',ignore_vxc=False, fixed_Zstar = False, use_full_ne_for_nf=False,
-		χ_type = 'Lindhard', gii_init_type = 'step', grid_spacing='quadratic'):
+		χ_type = 'Lindhard', gii_init_type = 'step', grid_spacing='quadratic', N_stencil_oneside = 2):
 		super().__init__(Z, A, name=name)
 		"""
 		Generates an average atom (rs=R) or neutral pseudo atom (R>>r).
@@ -134,7 +134,8 @@ class NeutralPseudoAtom(Atom):
 
 		# Instantiate 1-D grid and ThomasFermi
 		print("	Intializing grid")
-		self.grid = NonUniformGrid(rmin, R, Npoints, self.rs, spacing=grid_spacing)
+		self.N_stencil_oneside = N_stencil_oneside 
+		self.grid = NonUniformGrid(rmin, R, Npoints, self.rs, spacing=grid_spacing, N_stencil_oneside=N_stencil_oneside)
 		self.rws_index = np.argmin(np.abs(self.grid.xs - self.rs))
 		self.make_fast_n_TF()
 
@@ -351,7 +352,7 @@ class NeutralPseudoAtom(Atom):
 		δF_K/δρ = a/ρ nabla^2 (ρ) + b/ρ^2 |nabla(ρ)|^2 
 		"""
 		grad_n = self.grid.dfdx(ne)
-		laplace_ne = 1/self.grid.xs**2 * self.grid.dfdx(self.grid.xs**2 * grad_n)
+		laplace_ne = self.grid.A_laplace.dot(ne)
 
 		if self.gradient_correction=='K':
 			eta = self.TF.η_interp(ne)
@@ -492,8 +493,51 @@ class NeutralPseudoAtom(Atom):
 		self.Qion = self.Z  + self.grid.integrate_f( self.ρi )
 		# print("Qion = {}".format(self.Qion))
 
+	# def get_φe(self, ρ):
+	# 	"""
+	# 	Use solve_banded to solve Poisson Equation for φe using charge density ρ, which might be ρ = self.ρi - self.ne for example 
+	# 	Returns:
+	# 		float err: residual Ax-b 
+	# 	"""
+		
+	# 	def explicit_Ab(): #With BC at core and edge
+	# 		#### A ####
+	# 		### FIRST BULK VALUES ###
+	# 		A = -self.grid.A_laplace
+			
+	# 		dx= self.grid.dx
+	# 		x = self.grid.xs
+	# 		### BOUNDARIES ###
+	# 		A[0,0] =  1/dx[0] # Only E-field from electrons, zero from plasma ions
+	# 		A[0,1] = -1/dx[0]
+	# 		A[-1,-1] = 1 # Sets φe[-1]=0
+	# 		# A[-1,-1] = 1#/dx[-1] # Sets grad φe[-1]=0
+	# 		# A[-1,-2] = -1/dx[-1] # Sets grad φe[-1]=0
+			
+			
+	# 		b = np.zeros(self.grid.Nx)
+	# 		b[0]    = 8*np.pi/9*ρ[0]*x[0] # 8*np.pi/9*ρ[0]*x[0]
+	# 		# b[0]    = -20*self.grid.vols[0]*ρ[0]/x[1]**2
+	# 		b[-1]  =  5#(+self.get_Q() + self.Z )/self.R**2 #sets φe[-1]=0
+	# 		b[1:-1]= 4*π*ρ[1:-1]
+
+	# 		return A, b
+
+	# 	# Use function to create A, b
+	# 	A, b = explicit_Ab()
+	# 	self.Ab = A, b
+
+	# 	φe = tridiagsolve(A, b)
+	# 	# self.φe = jacobi_relaxation(A, b, self.φe, nmax=200) # quick smoothing, not to convergence
+
+	# 	# φe = φe - φe[-1]
+	# 	rel_errs = (np.abs(A @ φe - b)[:-1]/b[:-1])
+		
+	# 	return φe, rel_errs
+
 	def get_φe(self, ρ):
 		"""
+		5 diagonal
 		Use solve_banded to solve Poisson Equation for φe using charge density ρ, which might be ρ = self.ρi - self.ne for example 
 		Returns:
 			float err: residual Ax-b 
@@ -502,13 +546,12 @@ class NeutralPseudoAtom(Atom):
 		def explicit_Ab(): #With BC at core and edge
 			#### A ####
 			### FIRST BULK VALUES ###
-			A = -self.grid.matrix_laplacian()
+			A = -self.grid.A_laplace
 			
 			dx= self.grid.dx
 			x = self.grid.xs
 			### BOUNDARIES ###
-			A[0,0] =  1/dx[0] # Only E-field from electrons, zero from plasma ions
-			A[0,1] = -1/dx[0]
+			A[0,:] =  self.grid.A_dfdx[0,:] # get electric field at origin 
 			A[-1,-1] = 1 # Sets φe[-1]=0
 			# A[-1,-1] = 1#/dx[-1] # Sets grad φe[-1]=0
 			# A[-1,-2] = -1/dx[-1] # Sets grad φe[-1]=0
@@ -526,7 +569,7 @@ class NeutralPseudoAtom(Atom):
 		A, b = explicit_Ab()
 		self.Ab = A, b
 
-		φe = tridiagsolve(A, b)
+		φe = Ndiagsolve(A, b, self.N_stencil_oneside)
 		# self.φe = jacobi_relaxation(A, b, self.φe, nmax=200) # quick smoothing, not to convergence
 
 		# φe = φe - φe[-1]
@@ -548,20 +591,16 @@ class NeutralPseudoAtom(Atom):
 		def explicit_Ab(): #With BC at core and edge
 			#### A ####
 			### FIRST BULK VALUES ###
-			A = -self.grid.matrix_laplacian()
+			A = -self.grid.A_laplace
 			inner_diag_indices = ( np.arange(1, self.grid.Nx-1 ), np.arange(1, self.grid.Nx-1 ) )
 			A[inner_diag_indices] += self.φ_κ_screen**2 * np.ones( self.grid.Nx-2 )
 
 			dx= self.grid.dx
 			x = self.grid.xs
 			### BOUNDARIES ###
-			A[0,0] =  1/dx[0] # Only E-field from electrons, zero from plasma ions
-			A[0,1] = -1/dx[0]
+			A[0,:] =  self.grid.A_dfdx[0,:] # get electric field at origin 
 			A[-1,-1] = 1 # Sets φe[-1]=0
 
-			# A[-1,-1] = 1/dx[-1] # Sets grad φe[-1]=0
-			# A[-1,-2] = -1/dx[-1] # Sets grad φe[-1]=0
-			
 			
 			b = np.zeros(self.grid.Nx)
 			b[0]    = 8*np.pi/9*ρ[0]*x[0]
@@ -574,7 +613,7 @@ class NeutralPseudoAtom(Atom):
 		A, b = explicit_Ab()
 		self.Ab = A, b
 
-		φe = tridiagsolve(A, b)
+		φe = Ndiagsolve(A, b, self.N_stencil_oneside)
 		# self.φe = jacobi_relaxation(A, b, self.φe, nmax=200) # quick smoothing, not to convergence
 
 		# φe = φe - φe[-1]
@@ -582,37 +621,6 @@ class NeutralPseudoAtom(Atom):
 		
 		return φe, rel_errs
 	
-	# def get_φe_iet(self, ρ):
-	# 	# Setting up analytic ρ, φ for exact cancellation with central ion
-	# 	## iet space
-	# 	r, Z, rs, R = self.iet.r_array*self.rs, self.Z, self.rs, self.R
-	# 	λ=20/rs # rs
-	# 	Q = self.grid.integrate_f(ρ) # -Z
-	# 	self.iet.ρ0_r = λ**3*Q/(8*π) * np.exp(-λ*r) # analytically normalized to cancel Z
-
-	# 	### aa space
-	# 	r, Z, rs, R = self.grid.xs, self.Z, self.rs, self.R
-	# 	ε = 1e-10 # ε->0
-	# 	Γ = gammaincc(ε, r*λ)*gamma(ε) 
-	# 	self.φe0_iet = Q/r*(1-np.exp(-r*λ)) - 1/2*Q*λ**2*Γ
-	# 	self.ρ0_r_iet = λ**3*Q/(8*π) * np.exp(-λ*r)
-
-	# 	# numerical rest of charge
-	# 	self.iet.ρ_r = interp1d(self.grid.xs, ρ, bounds_error=False, fill_value='extrapolate')(self.iet.r_array*self.rs)
-	# 	self.iet.δρ_r = self.iet.ρ_r - self.iet.ρ0_r
-	# 	self.iet.δρ_k = self.iet.FT_r_2_k(self.iet.δρ_r)
-	# 	self.iet.δφe_k = 4*π*self.iet.δρ_k/(self.iet.k_array/self.rs)**2
-	# 	self.iet.δφe_r = self.iet.FT_k_2_r(self.iet.δφe_k)
-
-	# 	self.iet.δφe_r = self.iet.δφe_r
-	# 	δφe = interp1d(self.iet.r_array*self.rs, self.iet.δφe_r, bounds_error=False, fill_value='extrapolate')(self.grid.xs)
-	# 	φe = self.φe0_iet + δφe
-	# 	φe -= φe[-1]
-	# 	return φe, np.zeros_like(φe)
-
-	# def convert_density_grid_to_ftgrid(self, ρ):
-	# 	f_iet = interp1d(np.log(self.grid.xs), f, bounds_error=False, fill_value='extrapolate')(self.iet.r_array*self.rs)
-
 	def get_φe_iet(self, ρ):
 		# numerical rest of charge
 		Q_from_ρ = self.grid.integrate_f(ρ)

core/grids.py
@@ -1,8 +1,45 @@
 import numpy as np
 from scipy import fftpack
-
+from scipy.integrate import simpson
 
 π = np.pi
+
+
+def weights(z, x, n, m):
+    # From Bengt Fornbergs (1998) SIAM Review paper.
+    # Input Parameters:
+    # z - location where approximations are to be accurate
+    # x - grid point locations, found in x[0:n]
+    # n - one less than total number of grid points; n must not exceed the parameter nd
+    # m - highest derivative for which weights are sought
+    # Output:
+    # c - weights at grid locations x[0:n] for derivatives of order 0:m
+    
+    c = np.zeros((n+1, m+1))
+    c1 = 1.0
+    c4 = x[0] - z
+    for k in range(m+1):
+        for j in range(n+1):
+            c[j, k] = 0.0
+    
+    c[0, 0] = 1.0
+    for i in range(1, n+1):
+        mn = min(i, m)
+        c2 = 1.0
+        c5 = c4
+        c4 = x[i] - z
+        for j in range(i):
+            c3 = x[i] - x[j]
+            c2 *= c3
+            if j == i - 1:
+                for k in range(mn, 0, -1):
+                    c[i, k] = c1 * (k * c[i-1, k-1] - c5 * c[i-1, k]) / c2
+                c[i, 0] = -c1 * c5 * c[i-1, 0] / c2
+            for k in range(mn, 0, -1):
+                c[j, k] = (c4 * c[j, k] - k * c[j, k-1]) / c3
+            c[j, 0] = c4 * c[j, 0] / c3
+        c1 = c2
+    return c
 
 
 class OneDGrid():
@@ -157,10 +194,13 @@ class LinearGrid(OneDGrid):
 		return ffull
 
 class NonUniformGrid(OneDGrid):
-	def __init__(self,   xmin, xmax, Nx, rs, spacing='quadratic'):
+	def __init__(self,   xmin, xmax, Nx, rs, spacing='quadratic', N_stencil_oneside=2):
 		self.rs = rs
 		self.spacing = spacing
+		self.N_stencil_oneside = N_stencil_oneside
 		super().__init__(xmin, xmax, Nx)
+		self.make_dndx_matrices_byFornberg()
+
 	
 
 	def make_grid(self, frac_geometric = 0.25):
@@ -189,77 +229,203 @@ class NonUniformGrid(OneDGrid):
 		self.bulk_indcs = slice(1,-1)
 		self.grid_shape = self.Nx
 	
-	def integrate_f(self, f, begin_index = 0, end_index=None):
+	def integrate_f(self, f, end_index=None):
 		# f_geomfactor = 4*π*f*self.xs**2
 		# return np.sum(  (0.5*(f_geomfactor[1:] + f_geomfactor[:-1])*self.dx )[:end_index] )
-		integrated_slice = slice(begin_index, end_index)
-		return np.sum( (f*self.vols)[integrated_slice])
+		# integrated_slice = slice(None, end_index)
+		if end_index==None:
+			actual_end_index = end_index
+		else:
+			actual_end_index = end_index + 1
+		if end_index == 0:
+			return 0
+		else:
+			return simpson( (f*self.xs**2*4*π)[:actual_end_index], x=self.xs[:actual_end_index])
 
 
 	def dfdx(self, f):
 		"""
 		First order derivative of some function.
 		"""
-		fgrad = np.gradient(f, self.xs, edge_order=1)
-		
-		return fgrad
+		return self.A_dfdx.dot(f)
 
 	def d2fdx2(self, f):
 		"""
 		Second order derivative of some function f.
 		"""
-		fgrad2 = np.gradient(self.dfdx(f), self.xs, edge_order=2)
-		return fgrad2
+		return self.A_d2fdx2.dot(f)
 
-	def matrix_d2fdx2(self):
-		A = np.zeros((self.Nx, self.Nx)) # Is N x N
-		Abulk = A.copy()
-
-		dx= self.dx
-		x = self.xs
-
-		i = np.arange(self.Nx)[1:-1] # tri-diagonal index
-
-		A[i, i+1] =   2/dx[i]  /(dx[i] + dx[i-1])   #1 above diagonal
-		A[i, i]   =  -2/(dx[i]*dx[i-1])           #diagonal
-		A[i, i-1] =   2/dx[i-1]/(dx[i] + dx[i-1])   #1 below diagonal
+	def laplace(self, f):
+		return self.A_laplace.dot(f)
 	
-		Abulk[1:-1,:] = A[1:-1,:]
-		return Abulk
-
-	def matrix_dfdx(self):
-		A = np.zeros((self.Nx, self.Nx)) # Is N x N
-		Abulk = A.copy()
+	def make_dndx_matrices_byFornberg(self):
+		num_left, num_right = self.N_stencil_oneside, self.N_stencil_oneside
+		A_1 = np.zeros((self.Nx, self.Nx)) # Is N x N, dfdx
+		A_2 = np.zeros((self.Nx, self.Nx)) # Is N x N, d2fdx2
 
 		dx= self.dx
 		x = self.xs
-
-		i = np.arange(self.Nx)[1:-1] # tri-diagonal index
-
-		A[i, i+1] =  dx[i-1]/dx[i]/(dx[i] + dx[i-1]) #1 above diagonal
-		A[i, i]   = (dx[i]-dx[i-1])/(dx[i]*dx[i-1])  #diagonal
-		A[i, i-1] = -dx[i]/dx[i-1]/(dx[i] + dx[i-1]) #1 below diagonal
-	
-		Abulk[1:-1,:] = A[1:-1,:]
-		return Abulk
-
-	def matrix_laplacian(self):
-		dx= self.dx
-		x = self.xs
-		i = np.arange(self.Nx)[1:-1] # tri-diagonal index
-
-		# 2/r φ'
-		A1 = np.zeros((self.Nx, self.Nx)) # Is N x N
-		A1[i, i+1] = 2/x[i] *  dx[i-1]/dx[i]/(dx[i] + dx[i-1]) #1 above diagonal
-		A1[i, i]   = 2/x[i] * (dx[i]-dx[i-1])/(dx[i]*dx[i-1])  #diagonal
-		A1[i, i-1] = 2/x[i] * -dx[i]/dx[i-1]/(dx[i] + dx[i-1]) #1 below diagonal
+		N = len(self.dx)
 		
-		# φ"		
-		A2 = self.matrix_d2fdx2()
+		for i in np.arange(self.Nx)[num_left:-num_right]: # tri-diagonal index
+			weight_slice = slice(i-num_left, i+num_right+1)
+			center_weights = weights(x[i], x[weight_slice], num_left+num_right, 2).T
+			A_1[i, weight_slice] = center_weights[1] # first derivative coefficients
+			A_2[i, weight_slice] = center_weights[2] # second derivative coefficients
+		
+		for i in range(0, num_left):
+			total_points = num_right + i + 1
+			weight_slice = slice(None,total_points)
+			left_edge_weights = weights(x[i], x[weight_slice], total_points-1, 2).T
+			A_1[i, weight_slice ] = left_edge_weights[1] # first derivative coefficients 
+			A_2[i, weight_slice ] = left_edge_weights[2] # second derivative coefficients
 
-		A = A1 + A2 
-		Abulk =  np.zeros((self.Nx, self.Nx)) # Is N x N
-		Abulk[1:-1,:] = A[1:-1,:]
+		for i in range(0, num_right):
+			total_points = num_left + i + 1
+			weight_slice = slice(-total_points, None)
+			right_edge_weights = weights(x[N-i], x[weight_slice], total_points-1, 2).T
+			A_1[N-i, weight_slice ] = right_edge_weights[1] # first derivative coefficients 
+			A_2[N-i, weight_slice ] = right_edge_weights[2] # second derivative coefficients
+
+		self.A_dfdx = A_1
+		self.A_d2fdx2 = A_2
+		self.A_laplace = 2/x[:,np.newaxis] * A_1 + A_2
 
 
-		return Abulk
+	# def matrix_d2fdx2(self):
+	# 	A = np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk = A.copy()
+
+	# 	dx= self.dx
+	# 	x = self.xs
+
+	# 	i = np.arange(self.Nx)[1:-1] # tri-diagonal index
+
+	# 	A[i, i+1] =   2/dx[i]  /(dx[i] + dx[i-1])   #1 above diagonal
+	# 	A[i, i]   =  -2/(dx[i]*dx[i-1])           #diagonal
+	# 	A[i, i-1] =   2/dx[i-1]/(dx[i] + dx[i-1])   #1 below diagonal
+	
+	# 	Abulk[1:-1,:] = A[1:-1,:]
+	# 	return Abulk
+
+	# def matrix_dfdx(self):
+	# 	A = np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk = A.copy()
+
+	# 	dx= self.dx
+	# 	x = self.xs
+
+	# 	i = np.arange(self.Nx)[1:-1] # tri-diagonal index
+
+	# 	A[i, i+1] =  dx[i-1]/dx[i]/(dx[i] + dx[i-1]) #1 above diagonal
+	# 	A[i, i]   = (dx[i]-dx[i-1])/(dx[i]*dx[i-1])  #diagonal
+	# 	A[i, i-1] = -dx[i]/dx[i-1]/(dx[i] + dx[i-1]) #1 below diagonal
+	
+	# 	Abulk[1:-1,:] = A[1:-1,:]
+	# 	return Abulk
+
+
+	# def matrix_d2fdx2(self):
+	# 	A = np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk = A.copy()
+
+	# 	dx = self.dx
+	# 	x = self.xs
+
+	# 	i = np.arange(self.Nx)[2:-2] # Five-diagonal index
+
+	# 	A[i, i+2] =   1 / (dx[i] * (dx[i] + dx[i-1] + dx[i-2]))  # 2 above diagonal
+	# 	A[i, i+1] =  -4 / (dx[i] * (dx[i] + dx[i-1]))           # 1 above diagonal
+	# 	A[i, i]   =   6 / (dx[i] * dx[i-1])                     # diagonal
+	# 	A[i, i-1] =  -4 / (dx[i-1] * (dx[i] + dx[i-1]))         # 1 below diagonal
+	# 	A[i, i-2] =   1 / (dx[i-1] * (dx[i] + dx[i-1] + dx[i-2])) # 2 below diagonal
+
+	# 	Abulk[2:-2, :] = A[2:-2, :]
+	# 	return Abulk
+
+	# def matrix_dfdx(self):
+	# 	A = np.zeros((self.Nx, self.Nx))  # Is N x N
+	# 	Abulk = A.copy()
+
+	# 	dx = self.dx
+	# 	x = self.xs
+
+	# 	i = np.arange(self.Nx)[2:-2]  # Five-diagonal index
+
+	# 	for j in i:
+	# 		dx_m2 = x[j-2] - x[j]
+	# 		dx_m1 = x[j-1] - x[j]
+	# 		dx_0 = x[j] - x[j]
+	# 		dx_1 = x[j+1] - x[j]
+	# 		dx_2 = x[j+2] - x[j]
+
+	# 		A[j, j-2] =  1 / (2 * dx_m2 * (dx_m2 - dx_m1) * (dx_m2 - dx_0) * (dx_m2 - dx_1) * (dx_m2 - dx_2))
+	# 		A[j, j-1] = -8 / (2 * dx_m1 * (dx_m1 - dx_m2) * (dx_m1 - dx_0) * (dx_m1 - dx_1) * (dx_m1 - dx_2))
+	# 		A[j, j]   =  0  # This is typically zero for central differences.
+	# 		A[j, j+1] =  8 / (2 * dx_1 * (dx_1 - dx_m2) * (dx_1 - dx_m1) * (dx_1 - dx_0) * (dx_1 - dx_2))
+	# 		A[j, j+2] = -1 / (2 * dx_2 * (dx_2 - dx_m2) * (dx_2 - dx_m1) * (dx_2 - dx_0) * (dx_2 - dx_1))
+
+	# 	Abulk[2:-2, :] = A[2:-2, :]
+	# 	return Abulk
+
+	# def matrix_laplacian(self):
+	# 	"""
+	# 	nalba^2 in spherical: 1/r^2 d/dr ( r^2 d/dr )
+	# 	"""
+	# 	dx= self.dx
+	# 	x = self.xs
+	# 	i = np.arange(self.Nx)[1:-1] # tri-diagonal index
+
+	# 	# 2/r φ'
+	# 	A1 = np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	A1[i, i+1] = 2/x[i] *  dx[i-1]/dx[i]/(dx[i] + dx[i-1]) #1 above diagonal
+	# 	A1[i, i]   = 2/x[i] * (dx[i]-dx[i-1])/(dx[i]*dx[i-1])  #diagonal
+	# 	A1[i, i-1] = 2/x[i] * -dx[i]/dx[i-1]/(dx[i] + dx[i-1]) #1 below diagonal
+		
+	# 	# φ"		
+	# 	A2 = self.matrix_d2fdx2()
+
+	# 	A = A1 + A2 
+	# 	Abulk =  np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk[1:-1,:] = A[1:-1,:]
+	# 	return Abulk
+
+	# def matrix_laplacian_old(self):
+	# 	"""
+	# 	nalba^2 in spherical: 1/r^2 d/dr ( r^2 d/dr )
+	# 	"""
+	# 	dx= self.dx
+	# 	x = self.xs
+	# 	i = np.arange(self.Nx)[1:-1] # tri-diagonal index
+
+	# 	# 2/r φ'
+	# 	A1 = 2/x[:, np.newaxis]*self.matrix_dfdx()
+	
+	# 	# φ"		
+	# 	A2 = self.matrix_d2fdx2()
+
+	# 	A = A1 + A2 
+	# 	Abulk =  np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk[1:-1,:] = A[1:-1,:]
+	# 	return Abulk
+
+	# def matrix_laplacian(self):
+	# 	"""
+	# 	nalba^2 in spherical: 1/r^2 d/dr ( r^2 d/dr )
+	# 	"""
+	# 	dx= self.dx
+	# 	x = self.xs
+	# 	i = np.arange(self.Nx)[1:-1] # tri-diagonal index
+
+	# 	#  r^2 d/dr
+	# 	A1 = self.xs**2*self.matrix_dfdx()
+		
+	# 	# 1/r^2 d/dr		
+	# 	A2 = self.xs**-2*self.matrix_dfdx()
+
+	# 	A = np.dot(A2, A1)
+	# 	Abulk =  np.zeros((self.Nx, self.Nx)) # Is N x N
+	# 	Abulk[1:-1,:] = A[1:-1,:]
+
+
+	# 	return Abulk